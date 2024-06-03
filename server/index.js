80aaf437c82a67bc70180303f098f503dead34ba
sohal13 <sohalr1305@gmail.com>
2024-06-03 13:49:07 +0530

    #

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from "express"
 import dotenv from "dotenv"
+import path from "path"
+import cors from 'cors'
 import { DBConnected } from "./DataBase/DBConnect.js";
 import UserRout from '../server/Route/UserRout.js'
-import cors from 'cors'
 
 dotenv.config();
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -12,6 +14,13 @@ app.use(express.json());
 app.use(cors("*"));
 
 app.use('/api/user',UserRout)
+
+app.use(express.static(path.join(__dirname,"/client/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"client","dist","index.html"))
+})
+
 
 
 app.listen(process.env.PORT || 4000, () => {
