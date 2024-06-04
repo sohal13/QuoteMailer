a5432788159582e28b67e6a0eaa43016477ee534
sohal13 <sohalr1305@gmail.com>
2024-06-05 02:24:03 +0530

    #

--- server/RouteControler/RoutControler.js
+++ server/RouteControler/RoutControler.js
@@ -2,69 +2,70 @@ import Quote from "../Schema/QuoteSchema.js";
 import User from "../Schema/UserSchema.js";
 import cron from 'node-cron'
 import nodeMailer from 'nodemailer'
+import dotenv from 'dotenv'
 
+dotenv.config();
 
+const transPorter = nodeMailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user:process.env.EMAIL_USER,
+        pass:process.env.EMAIL_PASS
+    },
+});
 
-export const getUserData = async (req, res) => {
-    
-    const transPorter = nodeMailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user:process.env.EMAIL_USER,
-            pass:process.env.EMAIL_PASS
-        },
-    });
-    
-    const sendMail = (email, quote) => {
-        console.log("In sending Email");
-        const mailOptions = {
-            from: process.env.EMAIL_USER,
-            to: email,
-            subject: 'Quote Of The Day 😁!!',
-            html: `
-            <div style="font-size: 16px; font-family: Arial;">
-              <blockquote style="font-size: 20px;">"${quote.text}"</blockquote>
-              <p style="font-size: 14px;">- ${quote.author || 'Unknown'}</p>
-            </div>
-            `
+const sendMail = (email, quote) => {
+    console.log("In sending Email");
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: email,
+        subject: 'Quote Of The Day 😁!!',
+        html: `
+        <div style="font-size: 16px; font-family: Arial;">
+          <blockquote style="font-size: 20px;">"${quote.text}"</blockquote>
+          <p style="font-size: 14px;">- ${quote.author || 'Unknown'}</p>
+        </div>
+        `
+    }
+    transPorter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Email sent: ' + info.response);
         }
-        transPorter.sendMail(mailOptions, (err, info) => {
-            if (err) {
-                console.log(err);
+    })
+}
+
+const getRandomQuote = async (user) => {
+    console.log("In Getting RandomQuote");
+    const unsendquote = await Quote.find({ _id: { $nin: user.sendedQuotes } });
+    if (unsendquote.length === 0) {
+        return null;
+    }
+    const randomIndex = Math.floor(Math.random() * unsendquote.length)
+    return unsendquote[randomIndex];
+}
+
+const scheduleEmail = async (user) => {
+    try {
+        console.log("In Scheduling Email");
+        const [hour, minute] = user.timeing.split(':');
+        cron.schedule(`${minute} ${hour} * * *`, async () => {
+            const quote = await getRandomQuote(user)
+            if (quote) {
+                sendMail(user.email, quote);
+                user.sendedQuotes.push(quote._id);
+                await user.save();
             } else {
-                console.log('Email sent: ' + info.response);
+                console.log('No more new quotes to send to user:', user.email);
             }
         })
+    } catch (error) {
+        console.log(error);
     }
-    
-    const getRandomQuote = async (user) => {
-        console.log("In Getting RandomQuote");
-        const unsendquote = await Quote.find({ _id: { $nin: user.sendedQuotes } });
-        if (unsendquote.length === 0) {
-            return null;
-        }
-        const randomIndex = Math.floor(Math.random() * unsendquote.length)
-        return unsendquote[randomIndex];
-    }
-    
-    const scheduleEmail = async (user) => {
-        try {
-            console.log("In Scheduling Email");
-            const [hour, minute] = user.timeing.split(':');
-            cron.schedule(`${minute} ${hour} * * *`, async () => {
-                const quote = await getRandomQuote(user)
-                if (quote) {
-                    sendMail(user.email, quote);
-                    user.sendedQuotes.push(quote._id);
-                    await user.save();
-                } else {
-                    console.log('No more new quotes to send to user:', user.email);
-                }
-            })
-        } catch (error) {
-            console.log(error);
-        }
-    }
+}
+
+export const getUserData = async (req, res) => {
     
     try {
         const { email, timeing } = req.body;
